b7ec0bf838147725361110109034ae3e584222ec
이승욱 <lsw5410@naver.com>
2024-05-20 11:28:48 +0900

    feat : 메인페이지 로그인 카드 로그인 상태 식별 가능하도록 변경

--- vue/src/views/Dashboard.vue
+++ vue/src/views/Dashboard.vue
@@ -3,13 +3,14 @@ import MiniDealSearchBar from "./components/MiniDealSearchBar.vue";
 import MiniQnABoard from "./components/MiniQnABoard.vue";
 import MarkDealList from "./components/MarkDealList.vue";
 import MiniLogin from "./components/MiniLogin.vue";
+import ProfileCard from "./components/ProfileCard.vue";
 // import Carousel from "./components/Carousel.vue";
 import Map from "./components/Map.vue";
 
 import { ref } from 'vue';
 
 const aptDealList = ref([]);
-
+const isLogin = ref(true);
 
 
 </script>
@@ -19,7 +20,9 @@ const aptDealList = ref([]);
     
         <div class="row">
           <div class="col-lg-3 mb-lg ">
-            <MiniLogin/>
+            <button class="btn btn-primary" @click="isLogin = !isLogin">로그인 상태 바꾸기 </button>
+            <MiniLogin v-if="!isLogin"/>
+            <ProfileCard v-else/>
           </div>
 
           <div class="col-lg-4 mb-lg ">
