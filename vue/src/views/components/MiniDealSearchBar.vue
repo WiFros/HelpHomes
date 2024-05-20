8653a8dd68df2a6a2de18012cf72f692ef541044
이승욱 <lsw5410@naver.com>
2024-05-20 11:12:38 +0900

    feat : 메인페이지 로그인화면,게시판요약,관심매물 나오도록 수정

---
+++ vue/src/views/components/MiniDealSearchBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="bg-white border-radius-xl">
+      <div class="row justify-content-center ">
+            <div class="row col-md-12 mt-3">
+              <div class="form-group col-md-3 m-0">
+                <select class="form-select bg-secondary text-light" id="sido">
+                  <option value="">시도선택</option>
+                </select>
+              </div>
+              <div class="form-group col-md-3 m-0">
+                <select class="form-select bg-secondary text-light" id="gugun">
+                  <option value="">구군선택</option>
+                </select>
+              </div>
+              <div class="form-group col-md-3 m-0">
+                <select class="form-select bg-secondary text-light" id="dong">
+                  <option value="">동선택</option>
+                </select>
+              </div>
+              <div class="form-group col-md-3 m-0">
+                <button type="button" id="list-btn" class="btn btn-outline-primary">
+                  빠른검색
+                </button>
+              </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+  
+  
+  <script setup>
+
+
+  </script>
+  
+  
+  <style scoped>
+  </style>
---
+++ vue/src/views/components/MiniQnABoard.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="card">
+    <div class="card-header pb-0">
+      <h6>QnA 게시판</h6>
+    </div>
+
+    <div class="card-body px-0 pt-0 pb-2">
+      <div class="table-responsive p-0">
+        
+        <div class = "scrollable-table">
+        <table class="table align-items-center mb-0">
+      
+          </table>
+        </div>
+          <!-- Table Body -->
+          
+        <div class = "scrollable-content">
+        <table class="table align-items-center mb-0">
+          <tbody>
+            <tr v-for="qna in qnaList" :key="qna.num">
+              <td>
+                <div class="d-flex px-2 py-1">
+                  <div class="d-flex flex-column justify-content-center">
+                    <h6 class="mb-0 text-sm">
+                      <RouterLink :to="{
+                        name: 'QnaDetail',
+                        params: { num: qna.num }
+                      }">{{ qna.title }}
+                      </RouterLink>
+                    </h6>
+                  </div>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+import { selectAll} from '../../api/qna.js';
+import { RouterLink } from 'vue-router';
+
+const qnaList = ref([]);
+
+
+onMounted(() => {
+  selectAll(
+    (response) => {
+      console.log(response.data);
+      qnaList.value = response.data;
+    },
+    (error) => {
+      console.error(error);
+    }
+  );
+});
+
+</script>
+
+
+<style scoped>
+.scrollable-table {
+  overflow-x: auto;
+}
+.scrollable-content {
+  min-height: 240px; /* 내용이 표시될 최대 높이 */
+  max-height: 150px; /* 내용이 표시될 최대 높이 */
+  overflow-y: auto; /* 세로 스크롤 추가 */
+}
+</style>
