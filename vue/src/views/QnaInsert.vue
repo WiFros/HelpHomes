b9e8742eee33f08110de1a59363989dd5ac692a3
LeeSeungUk <tmddnr5410@gmail.com>
2024-05-23 21:10:15 +0900

    feat : insert수정

--- vue/src/views/QnaInsert.vue
+++ vue/src/views/QnaInsert.vue
@@ -1,24 +1,31 @@
 <script setup>
-import { ref } from "vue"
-import { useRouter } from "vue-router";
-import { insert } from "@/api/qna";
+import { ref, computed } from 'vue';
+import { useRouter } from 'vue-router';
+import { insert } from '@/api/qna';
 
-const writer = ref("")
-const title = ref("")
-const content = ref("")
+import { useStore } from 'vuex';
+
+const store = useStore();
+
+const user = computed(() => store.state.user);
+const writer = ref('');
+const title = ref('');
+const content = ref('');
 // const customers = ref([]) //전체 데이터 담을 배열
 
-
-const router = useRouter();//setup단계에서 찾아놓기
+const router = useRouter(); //setup단계에서 찾아놓기
 
 function qnaInsert() {
+  const maskName =
+    writer.value.substring(0, 1) + '*' + writer.value.substring(2);
   const new_qna = {
-    writer: writer.value,
+    writer: maskName,
     title: title.value,
     content: content.value,
-  }
+    writerId: user.value.id,
+  };
 
-  insert(new_qna)
+  insert(new_qna);
 
   alert('등록 되었습니다!');
 
@@ -26,35 +33,45 @@ function qnaInsert() {
   title.value = '';
   content.value = '';
 
-  //리스트 화면으로 넘어가기  
-  router.push({ path: '/qna' });//script 방식으로 넘어가기
+  //리스트 화면으로 넘어가기
+  router.push({ path: '/qna' }); //script 방식으로 넘어가기
 }
-
 </script>
 
 <template>
   <div class="mt-4 container-fluid">
-
-
-      
-  <div class="card">
-      <div class="card-header py-3" >
+    <div class="card">
+      <div class="card-header py-3">
         <h3>Q&A 등록</h3>
       </div>
 
       <div class="card-body">
         <div class="form-group">
-          <h5>제목:</h5> <input type="text" class="form-control" id="title" v-model="title" />
+          <h5>제목:</h5>
+          <input type="text" class="form-control" id="title" v-model="title" />
         </div>
 
         <div class="form-group">
-          <h5>내용:</h5> <input type="text" class="form-control" id="content" v-model="content" style=" height: 300px;" />
+          <h5>내용:</h5>
+          <input
+            type="text"
+            class="form-control"
+            id="content"
+            v-model="content"
+            style="height: 300px"
+          />
         </div>
 
         <div class="btn-group btn-center">
-          <input type="button" class="btn btn-primary" value="등록" id="btnInsert" @click="qnaInsert" />
+          <input
+            type="button"
+            class="btn btn-primary"
+            value="등록"
+            id="btnInsert"
+            @click="qnaInsert"
+          />
         </div>
-        </div>
+      </div>
     </div>
   </div>
 </template>
