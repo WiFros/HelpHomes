d60e4ed59c0a201e9d0c0ab8edcbc55ba45c6b69
WiFros <39113029+WiFros@users.noreply.github.com>
2024-06-19 20:59:29 +0900

    update : 대용량 파일 삭제

--- resource/sql_temp/createSchem_ssafyweb.sql
+++
@@ -1,2 +0,0 @@
-drop database if exists ssafyweb;
-create schema ssafyweb;
--- resource/sql_temp/dealMark.sql
+++
@@ -1,9 +0,0 @@
-use ssafyweb;
-DROP TABLE IF EXISTS `dealMark`;
-CREATE TABLE `dealMark` (
-  `userId` INT NOT NULL,
-  `aptCode` BIGINT NOT NULL,
-  PRIMARY KEY (`userId`, `aptCode`),
-  FOREIGN KEY (`userId`) REFERENCES `users`(`id`),
-  FOREIGN KEY (`aptCode`) REFERENCES `houseinfo`(`aptCode`)
-);
